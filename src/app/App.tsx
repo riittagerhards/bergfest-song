c06d844fea969f6ef7b71592716fbbdcc77baff7
riittagerhards <rgerhards@outlook.com>
2021-11-04 17:16:37 +0100

    🎨 Minor changes on code in App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,11 +8,19 @@ function App(): JSX.Element {
   const [selectedName, setSelectedName] = useState<string | null>(null);
   console.log(selectedName);
 
+  let content;
+
+  if (selectedName) {
+    content = <p>Please add some songs</p>;
+  } else {
+    content = <RegistrationForm onSelectParticipantName={setSelectedName} />;
+  }
+
   return (
     <main className={styles.container}>
       <div>
-        <Title text="Bergfest" />
-        <RegistrationForm onSelectParticipantName={setSelectedName} />
+        <Title text={selectedName ? `Welcome ${selectedName}` : 'Bergfest'} />
+        {content}
       </div>
       <Image
         url="https://images.pexels.com/photos/1071878/pexels-photo-1071878.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"
